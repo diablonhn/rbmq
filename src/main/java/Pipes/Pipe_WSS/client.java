a59b19d116de7911da8f1b67a4a563d8af88e7b0
nam <nam@caucho.com>
2016-05-10 02:37:54 -0700

    remove debugging messages

--- src/main/java/Pipes/Pipe_WSS/client.java
+++ src/main/java/Pipes/Pipe_WSS/client.java
@@ -20,37 +20,37 @@ public class client implements ServiceWebSocket<String, String>{
 
 	//@Inject
 	//RBMQfooService fs;
-	
+
 	@Inject @Service("pipe:///messages")
 	Pipes<String> _pipes;
 
 	private Pipe<String> _pipeIn;
-	
+
 //	private long id = new Random().nextLong();
-	
+
 	public void open(WebSocket<String> ws) {
-		
+
 		ResultPipeIn<String> resultPipe = Pipe.in((String x) -> {
-			System.out.println("Lambda called back: " + x);
+			//System.out.println("Lambda called back: " + x);
 			ws.next(x);
 		});
-		
-		//_pipeIn = resultPipe.pipe();
-		
-		_pipes.subscribe(resultPipe);
-		
-		System.out.println("INSIDE CLIENT.java WS");
 
-		/*	
-		 * 	
+		//_pipeIn = resultPipe.pipe();
+
+		_pipes.subscribe(resultPipe);
+
+		//System.out.println("INSIDE CLIENT.java WS");
+
+		/*
+		 *
 		fs.Receive((x,e) -> {
 			ws.next(x);
 		});
-		 * 
+		 *
 		 * fs.Send((x,e) -> {
 		System.out.println(x);
-	});	
-		
+	});
+
 		try {
 			fs.Register( id, (x,e,isend) -> {
 				//send back to client
@@ -67,18 +67,18 @@ public class client implements ServiceWebSocket<String, String>{
 	public void close(WebSocketClose code, String msg, WebSocket<String> webSocket) throws IOException{
 		_pipeIn.close();
 	}
-	
-	
+
+
 	/*
 	@Override
 	public void next(String value, WebSocket<String> webSocket) throws IOException {
 		System.out.println("Next");
 
 		if (value.equalsIgnoreCase("register")){
-			
-			
+
+
 			/*
-			
+
 			try {
 				fs.Register(id, (x,e,isend) ->{
 
@@ -88,7 +88,7 @@ public class client implements ServiceWebSocket<String, String>{
 			catch (IOException e) {
 
 				e.printStackTrace();
-			}	
+			}
 		}
 		else if(value.equalsIgnoreCase("unsubscribe")){
 			fs.remove(id);
@@ -105,7 +105,7 @@ public class client implements ServiceWebSocket<String, String>{
 	@Override
 	public void next(String value, WebSocket<String> webSocket) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 
